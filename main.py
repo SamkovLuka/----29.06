bfe67cecff6bf25bf13992bdc8fb39b07f607f17
SamkovLuka <staddycase@gmail.com>
2024-06-22 16:48:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,8 @@ print(tx.replace(wd,fn))
 #завдання 3
 
 txt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-print(txt.count(".",0,10000))
+n = txt.count(".",0,10000))
+m = txt.count("!",0,10000))
+g = txt.count("?",0,10000))
+w = n + m + g
+print(f"кількість речень - {w}")
